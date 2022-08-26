fd0a232b7c7744d04a5a1d9c82c9f10574451093
Pere Montpeo <pere.montpeo@decathlon.com>
2022-08-26 11:30:18 +0200

    feature 404 component

---
+++ src/routes/404/index.js
@@ -0,0 +1,23 @@
+import { h } from 'preact'
+import Hero from '../../components/hero'
+
+const Espais = () => {
+  return (
+    <div
+      style={{
+        background:
+          'linear-gradient(to top, rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(/assets/images/banner.png)',
+        backgroundAttachment: 'fixed',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }}
+      class="scroll-smooth"
+    >
+      <Hero isLanding={false} title="Ups!" subtitle="Sembla que t'has equivoat de porta." cta={{text:'Tonar a l\'inici', link:"/"}}/>
+
+    </div>
+  )
+}
+
+export default Espais
