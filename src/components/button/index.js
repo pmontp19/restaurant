7b78cada373ee5f8fa181c581232a29f7aa7b678
Pere Montpeo <pere.montpeo@decathlon.com>
2022-08-27 13:54:09 +0200

    fix: outline button on hover

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,13 +2,13 @@ import { h } from 'preact'
 
 const Button = ({ children, variant, ...props }) => {
   const commonClasses =
-    'px-8 py-2.5 mr-2 mb-2 font-bold rounded-sm text-sm tracking-wider no-underline text-ellipsis appearance-none cursor-pointer inline-flex text-center items-center uppercase justify-center overflow-hidden align-baseline'
+    'px-8 py-2.5 mr-2 mb-2 font-bold rounded-sm text-sm tracking-wider no-underline transition-colors text-ellipsis appearance-none cursor-pointer inline-flex text-center items-center uppercase justify-center overflow-hidden align-baseline'
   if (variant === 'outline') {
     return (
       <a
         href={props.href}
         class={
-          'bg-transparent hover:bg-red-800 text-white hover:text-white py-2 px-4 border border-white hover:border-red-800 ' +
+          'bg-transparent hover:bg-slate-50 text-white hover:text-slate-700 py-2 px-4 border border-white hover:border-slate-50 ' +
           commonClasses
         }
       >
