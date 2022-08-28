3e06ab8fc25f6b7b1fb5b2282f2e3d84cf0c8e4c
Pere Montpeo <pere.montpeo@decathlon.com>
2022-08-29 01:08:21 +0200

    routes updated

--- src/components/app.js
+++ src/components/app.js
@@ -7,14 +7,18 @@ import Footer from './footer';
 // Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
 import Espais from '../routes/espais';
+import Historia from '../routes/historia';
 import NotFound from '../routes/404';
+import CookieConsent from './cookies';
 
 const App = () => (
 	<div id="app">
+		<CookieConsent />
 		<Header />
 		<Router>
 			<Home path="/" />
 			<Espais path="/espais"/>
+			<Historia path="/historia"/>
 			<NotFound default />
 		</Router>
 		<Footer />
